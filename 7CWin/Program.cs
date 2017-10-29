8f2831f40bc27b3d922295522b7b009f44661dad
Haskaalo <haskaalo@gmail.com>
2017-10-29 17:35:28 -0400

    will it fix?

--- 7CWin/Program.cs
+++ 7CWin/Program.cs
@@ -19,6 +19,7 @@ namespace _7CWin
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
                 .Build();
     }
